1102e952b75c0a0ab1c7c191bf00981f30179a33
Brien01 <abdg2006mhpnhs@gmail.com>
2020-11-12 22:34:58 +0800

    fix: PLS

--- src/musicord.js
+++ src/musicord.js
@@ -3,50 +3,14 @@ import Musicord from './classes/Client.js'
 
 import { logInit, logError } from './utils/logger.js'
 
-export const run = async() => {
-	try {
-		/** Process Error: unhandledRejection */
-		process.on('unhandledRejection', async (error) => {
-			await logError('Main', 'unhandledRejection', error)
-		})
-		/** Process Error: uncaughtException */
-		process.on('uncaughtException', async (error) => {
-			await logError('Main', 'uncaughtException', error)
-		})
-	} catch(error) {
-		await logError('Main', 'process error handler', error)
-		process.exit(1)
-	}
+const bot = new Musicord(config.clientOpts, config.playerOpts);
 
-	try {
-		if (config.token) {
-			await logInit('Main', 'Launching Musicord...')
-			await musicord()
-		}
-	} catch(error) {
-		await logError('Main', 'invalid or unknown token', error)
-		process.exit(1)
-	}
-}
+process.on('unhandledRejection', async (error) => {
+	await logError('Main', 'unhandledRejection', error)
+})
+/** Process Error: uncaughtException */
+process.on('uncaughtException', async (error) => {
+	await logError('Main', 'uncaughtException', error)
+})
 
-const musicord = async () => {
-	const bot = new Musicord(config.clientOpts, config.playerOpts);
-
-	/** Login our bot */
-	try {
-		await logInit('Main', 'Waiting for login...')
-		await bot.login(config.token);
-	} catch(error) {
-		await logError('Main', 'unable to login', error)
-		process.exit(1)
-	}
-
-}
-
-/** Run the whole bot */
-try {
-	run()
-	logInit('Main', 'Bot initialized')
-} catch(error) {
-	logError('Main', 'Bot failed to run', error)
-}
+bot.login(config.token);
