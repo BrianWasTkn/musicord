dc8f579ff1d8b9d55d82f1d9fd741bfc19158472
Brien01 <abdg2006mhpnhs@gmail.com>
2020-11-13 20:58:40 +0800

    fix: prefix

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
 	token: 'Njg2OTY5MDIwMzg1MzI5MTgy.Xme7wQ.8NNvk2zlQ6I08eQcqDNRD7OxlZs',
-	prefix: '>',
+	prefix: ['m!', '>', '?'],
 	clientOpts: {
 		status: 'STREAMING',
 		activity: {
