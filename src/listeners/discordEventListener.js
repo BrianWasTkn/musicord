9653cfedc6851a0135c68f208950d5874fbde2d8
Brien01 <abdg2006mhpnhs@gmail.com>
2020-11-13 20:45:52 +0800

    test: instance of new listeners

---
+++ src/listeners/discordEventListener.js
@@ -0,0 +1,14 @@
+import { logInit, logError } from '../utils/logger.js'
+
+export async function run(bot) {
+	try {
+		bot
+		.on('ready', () => logInit('Discord', `${bot.user.tag} is now ready to play some beats!`))
+		.on('debug', (info) => logInit('Discord', info))
+		.on('warn', (info) => logError('Discord', 'A warning emitted by Discord', info))
+		.on('error', (error) => logError('Discord', 'An error emitted by Discord', error))
+		.on('rateLimit', (rateLimit) => logError('Discord', 'Rate Limit warning', rateLimit))
+	} catch(error) {
+		logError('Listener', 'Unable to initiate discord event listeners', error)
+	}
+}
