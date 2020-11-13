59284748c49b58dc73c920d0231cfad322ccf84b
Brien01 <abdg2006mhpnhs@gmail.com>
2020-11-13 23:34:08 +0800

    fix: change

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
 	token: 'Njg2OTY5MDIwMzg1MzI5MTgy.Xme7wQ.8NNvk2zlQ6I08eQcqDNRD7OxlZs',
-	prefix: ['m!', '>', '?'],
+	prefix: ['m!', 'c!', 'crib '],
 	clientOpts: {},
 	playerOpts: {
 		searchSongs: true,
