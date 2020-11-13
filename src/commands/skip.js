9f61e5e1188fa9feab07c2cccc6f0dc7c6265ac2
Brien01 <abdg2006mhpnhs@gmail.com>
2020-11-13 23:42:19 +0800

    fix: visual

--- src/commands/skip.js
+++ src/commands/skip.js
@@ -2,7 +2,7 @@ import Command from '../classes/Command.js'
 
 export default new Command({
 	name: 'skip',
-	aliases: ['playnext'],
+	aliases: ['playnext', 's'],
 	description: 'skip the current track',
 	usage: 'command',
 	cooldown: 3e3,
@@ -16,14 +16,9 @@ export default new Command({
 	}
 
 	/** Do the thing */
-	const queue = await bot.player.skip(message);
-	return {
-		author: {
-			name: 'Track Skipped',
-			iconURL: queue.songs[0].user.avatarURL()
-		},
-		fields: [
-			{ name: 'Now Playing', value: `\`${queue.songs[0].name}\`` }
-		]
+	try {
+		await bot.player.skip(message)
+	} catch(error) {
+		console.error(error)
 	}
 })
