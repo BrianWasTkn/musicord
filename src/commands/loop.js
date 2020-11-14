b64f9c15beebfece47e60b69a56cd6b0147c51f6
Brien01 <abdg2006mhpnhs@gmail.com>
2020-11-14 16:21:03 +0800

    fix: use switch instead of ternary operator

--- src/commands/loop.js
+++ src/commands/loop.js
@@ -33,7 +33,17 @@ export default new Command({
 	/** Do the thing */
 	try {
 		const queue = await bot.player.setRepeatMode(message, repeatMode);
-		return queue.repeatMode ? queue.repeatMode === 0 ? 'Loop is now **__off__**' : queue.repeatMode === 1 ? 'Now looping **__current track__**' : 'Now looping **__queue__**'
+		switch(queue.repeatMode) {
+			case 2:
+				return 'Now looping **__queue__**'
+				break;
+			case 1:
+				return 'Now looping **__track__**'
+				break;
+			default:
+				return 'Loop is now **__off__**'
+				break;
+		}
 	} catch(error) {
 		logError('Command', 'Unable to skip the current track', error)
 	}
