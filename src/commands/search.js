913f21ae2b5828b47dfc436ec2dca277033c350b
Brien01 <abdg2006mhpnhs@gmail.com>
2020-11-15 22:09:01 +0800

    command: test instance of search cmd

--- src/commands/search.js
+++ src/commands/search.js
@@ -18,7 +18,7 @@ export default new Command({
 	/** Do the thing */
 	try {
 		const result = await bot.player.search(args.join(' '))
-		const found = result.map((song, index) => `**#${index + 1}:** [**${song.name}**](${song.url}) - **\`${song.formattedDuration}\`**`)
+		const found = result.map((song, index) => `**#${index + 1}:** [**${song.name}**](${song.url}) - **\`${song.formattedDuration}\`**`).slice(0, 5)
 		const msg = await message.channel.send({
 			embed: {
 				author: {
@@ -27,7 +27,7 @@ export default new Command({
 				},
 				color: 'BLUE',
 				fields: [
-					{ name: `**__${found.length} songs found__**`, value: found.slice(0, 5) },
+					{ name: `**__${found.length} songs found__**`, value: found },
 					{ name: '**__Instructions__**', value: '**Type the number of your choice.\nYou can type `cancel` to cancel your search.**' }
 				]
 			}
@@ -36,13 +36,30 @@ export default new Command({
 		try {
 			const choice = await message.channel.awaitMessages(m => m.author.id === message.author.id, {
 				max: 1,
-				time: 3e4,
+				time: 1e4,
 				errors: ['time']
 			})
-			if (!choice) throw Error('No Choice');
-			let index = parseInt(choice, 10);
-			await bot.player.play(message, result[index - 1].url)
-			await msg.delete()
+			// no answer
+			if (!choice.first()) {
+				throw new Error(`Next time if you're just gonna let me waste my time don't use this command again okay?`)
+			};
+			// index parsing
+			let index = parseInt(choice.first().content, 10);
+			// check if a number
+			if (isNaN(index) || index > results.length || index < 1) {
+        throw new Error(`Cannot parse ${index} as number.`)
+      };
+			// play it.
+			try {
+				await bot.player.play(message, result[index - 1].url)
+				try {
+					await msg.delete()
+				} catch(error) {
+					logError('Command', 'Cannot delete search embed', error)
+				}
+			} catch(error) {
+				logError('Command', 'Cannot play searched song', error)
+			}
 		} catch(error) {
 			logError('Command', 'An error in message Collector', error)
 			return 'You cancelled the search'
