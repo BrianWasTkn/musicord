cc8b348932c22ae9df6e578830ff6d697536298a
Brien01 <abdg2006mhpnhs@gmail.com>
2020-11-15 21:14:12 +0800

    new: search command attempt

---
+++ src/commands/search.js
@@ -0,0 +1,59 @@
+import Command from '../classes/Command.js'
+import { logError } from '../utils/logger.js'
+import { SearchResult } from 'distube'
+
+export default new Command({
+	name: 'search',
+	aliases: ['find'],
+	description: 'search a track',
+	usage: '<track>',
+	cooldown: 3e3,
+	music: true
+}, async (bot, message, args) => {
+	
+	/** Check Playing State */
+	const isPlaying = bot.player.isPlaying(message);
+	if (!isPlaying) {
+		return 'There\'s nothing playing in the queue.'
+	}
+
+	/** Args */
+	if (!args) {
+		return 'You need to search something.'
+	}
+
+	/** Do the thing */
+	try {
+		const result = bot.player.search(args.join(' '))
+		const found = result.map((song, index) => `**#${index + 1}:** [**${song.name}**](${song.url}) - **\`${song.formattedDuration}\`**`)
+		const msg = await message.channel.send({
+			embed: {
+				author: {
+					name: 'Search Results',
+					iconURL: message.guild.iconURL()
+				},
+				color: 'BLUE',
+				fields: [
+					{ name: `**__${result.length} songs found__**`, value: found },
+					{ name: '**__Instructions__**', value: '**Type the number of your choice.\nYou can type `cancel` to cancel your search.**' }
+				]
+			}
+		})
+		/** Awaited Message */
+		try {
+			const choice = message.channel.awaitMessages(m => m.author.id === message.author.id, {
+				max: 1,
+				time: 3e4,
+				errors: ['time']
+			})
+			if (!choice) throw Error('No Choice');
+			let index = parseInt(choice, 10);
+			await bot.player.play(message, results[index - 1].url)
+		} catch(error) {
+			logError('Command', 'An error in message Collector', error)
+			return 'You cancelled the search'
+		}
+	} catch(error) {
+		logError('Command', 'Unable to skip the current track', error)
+	}
+})
