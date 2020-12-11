e36e87ffcac8b1979640e9d90637f2e83e5c1a95
BrianWasTkn <abdg2006mhpnhs@gmail.com>
2020-12-11 21:36:23 +0800

    e

--- src/cmds/UtilityCommands/ping.js
+++ src/cmds/UtilityCommands/ping.js
@@ -1,20 +1,18 @@
 import { Command } from '../../lib/command/Command.js'
 
-export default async function run() {
-	return new Command(
-		async ({ ctx, msg, args }) => {
-			const { channel, guild: shard } = msg;
-			await channel.send(`here you go: \`${shard.ping}ms\``)
-	}, {
-		name: 'ping',
-		aliases: ['pong'],
-		description: 'View my latency for this guild.',
-		category: 'Utility',
-	}, {
-		cooldown: 5000,
-		rateLimit: 2,
-		enabled: true,
-		permLevel: 0,
-		nsfw: false
-	});
-}
+export default new Command(
+	async ({ ctx, msg, args }) => {
+		const { channel, guild: shard } = msg;
+		await channel.send(`here you go: \`${shard.ping}ms\``)
+}, {
+	name: 'ping',
+	aliases: ['pong'],
+	description: 'View my latency for this guild.',
+	category: 'Utility',
+}, {
+	cooldown: 5000,
+	rateLimit: 2,
+	enabled: true,
+	permLevel: 0,
+	nsfw: false
+});
