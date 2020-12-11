2c4365fcd3c4bfcd825cf1243d397e2f3fff2670
BrianWasTkn <abdg2006mhpnhs@gmail.com>
2020-12-11 23:32:43 +0800

    refactor: err

--- main/lib/processes/autoLottery.js
+++ main/lib/processes/autoLottery.js
@@ -68,7 +68,7 @@ exports.run = async ctx => {
 				],
 				footer: {
 					text: winner.id,
-					iconURL: winner.avatarURL()
+					iconURL: winner.avatarURL() || winner.user.avatarURL()
 				}
 			}});
 		}
