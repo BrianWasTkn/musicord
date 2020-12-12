6b62421b44fd41ee2d572c0ee1e6a154784bca66
BrianWasTkn <abdg2006mhpnhs@gmail.com>
2020-12-12 08:44:37 +0800

    refactor: undefined.tag lol

--- main/cmds/crib/b@n.js
+++ main/cmds/crib/b@n.js
@@ -7,9 +7,9 @@ module.exports = new Command(
 		let odds = Math.random();
 		if (odds > 0.5) {
 			if (!member.bannable) {
-				return await channel.send(`**${member.tag}** is about to get banned\n\nbut not bannable :thinking:`);
+				return await channel.send(`**${member.user.tag}** is about to get banned\n\nbut not bannable :thinking:`);
 			}
-			
+
 			let banned = await member.ban(member.id, `${msg.author.tag} ${msg.author.id} did this <_<`);
 			await msg.channel.send(`**oh no...** you b@nned **${member.tag}** with the odds of ${odds.toFixed(2)}! christ you're so road.`); 
 		} else {
