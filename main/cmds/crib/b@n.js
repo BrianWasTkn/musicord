1de2dbf82695e1ea881f8b67d8542b5ed5c95b70
BrianWasTkn <abdg2006mhpnhs@gmail.com>
2020-12-12 08:41:03 +0800

    fix: wrong params

--- main/cmds/crib/b@n.js
+++ main/cmds/crib/b@n.js
@@ -2,10 +2,15 @@ const Command = require('../../lib/command/Command.js');
 
 module.exports = new Command(
 	async ({ msg }) => {
+		let { channel } = msg;
 		let member = msg.mentions.members.first();
 		let odds = Math.random();
 		if (odds > 0.5) {
-			member = await member.ban(member.id, `${msg.author.tag} ${msg.author.id} did this <_<`);
+			if (!member.bannable) {
+				return await channel.send(`**${member.tag}** is about to get banned\n\nbut not bannable :thinking:`);
+			}
+			
+			let banned = await member.ban(member.id, `${msg.author.tag} ${msg.author.id} did this <_<`);
 			await msg.channel.send(`**oh no...** you b@nned **${member.tag}** with the odds of ${odds.toFixed(2)}! christ you're so road.`); 
 		} else {
 			await msg.channel.send(`${member.tag} is safe, yay! odds are \`${odds.toFixed(2)}\` btw.`);
