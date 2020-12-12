09397d9db04e27be8c5587cf59800d8797ac390e
BrianWasTkn <abdg2006mhpnhs@gmail.com>
2020-12-12 22:30:42 +0800

    fix[cmd]: fh cmd

--- main/cmds/crib/heist.js
+++ main/cmds/crib/heist.js
@@ -1,11 +1,11 @@
 const Command = require('../../lib/command/Command.js');
-
+const { Collection } = require('discord.js');
 module.exports = new Command(
 async ({ msg }) => {
 	const { channel, guild } = msg;
 	channel.send(`Type \`JOIN EVENT\` to join!`);
 	let filter = m => m.content.toLowerCase() === 'join event';
-	const entries = new (require('discord.js').Collection)();
+	const entries = new Collection();
 	const collector = await channel.createMessageCollector(filter, {
 		max: Infinity,
 		time: 30000,
@@ -26,28 +26,48 @@ async ({ msg }) => {
 			return col.first().reply(`Looks like you're alone.`);
 		}
 
-		let winners = col.random(Math.round(Math.random() * col.size));
-		let losers = col.filter(l => !winners.includes(l));
-		let coins = Math.floor(10e6 / winners.length);
+		let authors = col.map(m => m.author);
+		let success = [], fail = [], empty = [];
+		authors.array().forEach(a => {
+			let odds = Math.random();
+			if (odds > 0.66) {
+				success.push(`+ ${a.username} walked away with ${coins.toLocaleString()} coins!`);
+			} else if (odds > 0.33) {
+				empty.push(`- ${a.username} drowned in water, OOF.`);
+			} else {
+				fail.push(`# ${a.username} remained unseen.`);
+			}
+		});
 
-		await channel.send(`**${col.size}** ${col.size > 1 ? 'people are' : 'person is'} teaming up to win the grand prize.`);
-		await require('discord.js').Util.delayFor(Math.round(Math.random() * 5) * 1000);
+		let order = [success, fail, empty].sort(() => Math.random() - 0.5);
+		await channel.send([
+			...order[1].join('\n'), ...order[2].join('\n'), ...order[3].join('\n')
+		].join('\n'), {
+			code: 'diff'
+		});
+
+		// let winners = col.random(Math.round(Math.random() * col.size)).map(w => w.author.id);
+		// let losers = col.map(l => l.author.id).filter(l => !winners.includes(l));
+		// let coins = Math.floor(10e6 / winners.length);
+
+		// await channel.send(`**${col.size}** ${col.size > 1 ? 'people are' : 'person is'} teaming up to win the grand prize.`);
+		// await require('discord.js').Util.delayFor(Math.round(Math.random() * 3) * 1000);
 	
-		// if (winners) {}
-		winners = winners.length > 0 ? winners.map(w => {
-			return `+ ${w.author.username} grabbed ${coins.toLocaleString()} coins.`;
-		}) : [];
-		losers = losers.length > 0 ? losers.map(l => {
-			return `- ${l.author.username} died LOL`;
-		}) : [];
+		// // if (winners) {}
+		// winners = winners.length > 0 ? winners.map(w => {
+		// 	return `+ ${w.username} grabbed ${coins.toLocaleString()} coins.`;
+		// }) : [];
+		// losers = losers.length > 0 ? losers.map(l => {
+		// 	return `- ${l.username} died LOL`;
+		// }) : [];
 
-		if ([...winners, ...losers].length <= 0) {
-			return await channel.send(`\`${col.map(u => u.author.username).join('`, `')}\` failed the event.`);
-		} else {
-			await channel.send(`${winners.join('\n')}\n${losers.join('\n')}`, {
-				code: 'diff'
-			});
-		}
+		// if ([...winners, ...losers].length <= 0) {
+		// 	return await channel.send(`\`${col.map(u => u.author.username).join('`, `')}\` failed the event.`);
+		// } else {
+		// 	await channel.send(`${winners.join('\n')}\n${losers.join('\n')}`, {
+		// 		code: 'diff'
+		// 	});
+		// }
 
 		// let wString = winners.length > 1 ? (winners.map(w => `+ ${w.author.username} grabbed ${coins.toLocaleString()} coins!`).join('\n')) : [];
 		// let lString = losers.length > 1 ? (losers.map(l => `- ${l.author.username} died LOL`).join('\n')) : [];
