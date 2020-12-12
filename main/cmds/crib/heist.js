d79d846f5f8a0f6485b662831ae421c002790bb3
BrianWasTkn <abdg2006mhpnhs@gmail.com>
2020-12-12 21:24:54 +0800

    fix[cmd]: fh cmd

--- main/cmds/crib/heist.js
+++ main/cmds/crib/heist.js
@@ -13,9 +13,12 @@ module.exports = new Command(
 		});
 
 		collector.on('collect', async m => {
-			await m.react('💰');
-			if (entries.has(m.author.id)) return m.reply('you already joined.');
-			else entries.set(m.author.id, true);
+			if (entries.has(m.author.id)) {
+				return m.reply('you already joined.');
+			} else {
+				entries.set(m.author.id, true);
+				await m.react('💰');
+			}
 		}).on('end', async col => {
 			console.log(col.first());
 			// const random = arr => arr[Math.floor(Math.random() * arr.length)];
@@ -24,7 +27,7 @@ module.exports = new Command(
 			}
 			let winners = col.random(Math.floor(Math.random() * col.size));
 			let losers = col.filter(l => !winners.includes(winners));
-			let coins = Math.floor(10e6 / winners.size);
+			let coins = Math.floor(10e6 / winners.length);
 
 			await channel.send(`**${col.size}** ${col.size > 1 ? 'people are' : 'person is'} teaming up to win the grand prize.`);
 			await channel.send(winners ? winners.map(w => `+ ${w.author.username} grabbed ${coins.toLocaleString()}`).join('\n') : '# none', {
