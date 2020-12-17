0f6452c24822d0ee582010e59597a3c8dda9bef9
BrianWasTkn <abdg2006mhpnhs@gmail.com>
2020-12-17 12:55:38 +0800

    refactor[listener]: remove other prefix

--- main/lib/processes/commandListener.js
+++ main/lib/processes/commandListener.js
@@ -2,18 +2,12 @@ exports.run = async ctx => {
 	ctx.on('message', async msg => {
 		const { guild, channel, member } = msg;
 
-		let self = guild.members.cache.get(ctx.user.id);
-		let mention = `<@${self.nickname ? '!' : ''}${self.user.id}>`;
-		let mentioned = msg.content.startsWith(mention);
-		let prefixLength = (mentioned ? mention : ctx.config.main.prefix).length + 1;
-
-		if (
-			!msg.content.startsWith(ctx.config.main.prefix + ' ')
-		) {
+		let prefix = `${ctx.config.main.prefix} `;
+		if (!msg.content.startsWith(prefix)) {
 			return;
 		}
 
-		let [command, ...args] = msg.content.slice(prefixLength).trim().split(/ +/g);
+		let [command, ...args] = msg.content.slice(prefix.length).trim().split(/ +/g);
 		command = ctx.cmds.get(cmd) || ctx.cmdAliases.get(cmd);
 
 		if (command) {
