9eb49ccc9bd7aa8dab06f38b98daa3cae970cb51
BrianWasTkn <abdg2006mhpnhs@gmail.com>
2020-12-17 20:52:11 +0800

    feat[cmds]: add alex and mai san in cmc

--- main/lib/processes/cribSpawnEvents.js
+++ main/lib/processes/cribSpawnEvents.js
@@ -7,16 +7,14 @@ const randNum = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
 exports.run = async ctx => {
 	ctx.on('message', async msg => {
 		const events = readdirSync(join(__dirname, '..', 'spawns'));
-		console.log(events);
 		const spawn = require(join(__dirname, '..', 'spawns', random(events)));
-		console.log(spawn);
 
 		if (Math.random() > 0.6) {
 			if (msg.channel.id !== '695614620781641778') {
 				return;
+			} else {
+				await spawn.run(ctx, msg);
 			}
-			/* {Collection} results - the results */
-			await spawn.run(ctx, msg);
 		}
 	});
 }
