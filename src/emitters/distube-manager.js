bb4ff3b072dc69b37a1b8fec6535e279b0c79d66
BrianWasTkn <abdg2006mhpnhs@gmail.com>
2020-12-19 17:52:15 +0800

    feat[core]: add distube manager

--- src/emitters/distube-manager.js
+++ src/emitters/distube-manager.js
@@ -13,6 +13,26 @@ export async function run() {
 				{ inline: true, name: 'Requested by', value: song.user.tag },
 				{ inline: true, name: 'Views', value: `\`${song.views.toLocaleString()}\`` },
 			],
+			timestamp: Date.now(),
+			footer: {
+				text: client.user.username,
+				iconURL: client.user.avatarURL()
+			}
+		}});
+	})
+	.on('addSong', async (message, queue, song) => {
+		const { channel, client } = message;
+		await channel.send({ embed: {
+			title: 'Added to Queue',
+			thumbnail: { url: song.thumbnail },
+			color: 'ORANGE',
+			description: `Added [**__${song.name}__**](${song.url}) into the queue.`,
+			fields: [
+				{ inline: true, name: 'Duration', value: `\`${song.formattedDuration}\`` },
+				{ inline: true, name: 'Added by', value: song.user.tag },
+				{ inline: true, name: 'Views', value: `\`${song.views.toLocaleString()}\`` },
+			],
+			timestamp: Date.now(),
 			footer: {
 				text: client.user.username,
 				iconURL: client.user.avatarURL()
