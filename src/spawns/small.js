39cd5717745fa75daefd9c9041888153e1f1b83d
BrianWasTkn <abdg2006mhpnhs@gmail.com>
2021-01-02 21:36:25 +0800

    feat[core]: add new spawner"

---
+++ src/spawns/small.js
@@ -0,0 +1,22 @@
+exports.config = {
+	odds: 50,
+	cooldown: 15,
+	enabled: true,
+	timeout: 30000,
+	entries: 5,
+	rewards: {
+		min: 1000,
+		max: 10000
+	} 
+}
+
+exports.visuals = {
+	emoji: '<:memerBlue:729863510330310727>',
+	type: 'COMMON',
+	title: 'Heist Money',
+	description: 'Do you need some money?',
+	strings: [
+		'yes', 'mhm', 'i do', 'yep', 'lol okay',
+		'for heist pls', 'i ned money'
+	]
+}
