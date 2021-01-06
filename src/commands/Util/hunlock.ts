4180e1c94dae4356fc9943a4ac9a018b1aa92d96
BrianWasTkn <abdg2006mhpnhs@gmail.com>
2021-01-06 22:29:39 +0800

    feat: add commands

--- src/commands/Util/hunlock.ts
+++ src/commands/Util/hunlock.ts
@@ -4,8 +4,8 @@ import { Message, Role, Snowflake } from 'discord.js'
 export default class Util extends Command implements LavaCommand {
   public client: LavaClient;
   public constructor() {
-    super('hlock', {
-      aliases: ['hlock', 'hl'],
+    super('hunlock', {
+      aliases: ['hunlock', 'hul'],
       channel: 'guild',
       userPermissions: 'MANAGE_CHANNELS',
       args: [ 
@@ -17,12 +17,12 @@ export default class Util extends Command implements LavaCommand {
   public async exec(_: Message, args: any): Promise<Message> {
     const { role }: { role: Role } = args;
     const { channel }: any = _;
-    const perms = { SEND_MESSAGES: null };
+    const perms = { SEND_MESSAGES: true };
     const edit = (id: Snowflake, perms: any): any => {
       return channel.updateOverwrite(id, perms);
     }
 
     const chan: any = await edit(role.id, perms);
-    return chan.send(`Locked for **${role.name}**`);
+    return chan.send(`Unlocked for **${role.name}**`);
   }
 }
