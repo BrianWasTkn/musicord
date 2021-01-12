330e9baae172ef8bd9ea7db914c56be75ce02ba1
BrianWasTaken <56699623+BrianWasTkn@users.noreply.github.com>
2021-01-13 04:22:20 +0800

    feat: request from idiot, your wish came true

--- src/spawns/small-monies.ts
+++ src/spawns/small-monies.ts
@@ -1,22 +1,22 @@
 import { SpawnConfig, SpawnVisuals } from 'discord-akairo'
 
 export const config: SpawnConfig = {
-	odds: 10,
+	odds: 25,
 	cooldown: 15,
 	enabled: true,
 	timeout: 5000,
 	entries: 5,
 	rewards: {
-		min: 1000,
-		max: 1000
+		min: 500000,
+		max: 2500000
 	} 
 }
 
 export const visuals: SpawnVisuals = {
 	emoji: '<:memerBlue:729863510330310727>',
 	type: 'COMMON',
-	title: 'Heist Money',
-	description: 'Do you need some money to enter a heist?',
+	title: 'Financial Support',
+	description: 'Do you need some money?',
 	strings: [
 		'yes', 'mhm', 'i do', 'yep', 'lol okay',
 		'for heist pls', 'i ned money'
