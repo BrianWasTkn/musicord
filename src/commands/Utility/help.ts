94d3673f645d866fca8dd03047ba7ad0326274ff
BrianWasTkn <abdg2006mhpnhs@gmail.com>
2021-01-21 22:43:56 +0800

    e

--- src/commands/Utility/help.ts
+++ src/commands/Utility/help.ts
@@ -1,4 +1,4 @@
-import { Message, MessageEmbed } from 'discord.js'
+import { Message, MessageEmbed, EmbedField } from 'discord.js'
 import Lava from 'discord-akairo'
 
 
@@ -17,7 +17,29 @@ export default class Utility extends Lava.Command {
 		});
 	}
 
+	private mapCommands(): EmbedField[] {
+		const commands = this.handler.modules.array();
+		const categories = [ ...new Set(commands.map(c => c.categoryID))];
+		return categories.map((c: string) => ({
+			name: c, inline: false,
+			value: `\`${commands.filter(c => c.categoryID).map(c => c.aliases[0]).join('`, `')}\``
+		}));
+	}
+
 	public async exec(_: Message, args: any): Promise<Message> {
+		const command = args.query;
+		if (!command) {
+			const fields: EmbedField[] = this.mapCommands();
+			return _.channel.send({ embed: {
+				title: 'Lava Commands',
+				color: 'RANDOM',
+				fields, footer: {
+					text: `${this.handler.modules.size} commands`,
+					iconURL: this.client.user.avatarURL()
+				}
+			}});
+		}
+
 		if (args.query) {
 			const command: Lava.Command = args.query;
 			return _.channel.send({ embed: {
