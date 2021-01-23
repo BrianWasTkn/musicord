8b89e9b471a79be040fef884909a4d2c41151611
BrianWasTkn <abdg2006mhpnhs@gmail.com>
2021-01-23 17:35:13 +0800

    chore[spawners]: rewrite spawners; add spam events, first2react events

--- src/structures/Spawn.ts
+++
@@ -1,9 +0,0 @@
-import discord from 'discord.js'
-import Lava from 'discord-akairo'
-
-class Spawner {
-	public client: Lava.Client;
-	public constructor(client: Lava.Client) {
-		this.client = client;
-	}
-}
