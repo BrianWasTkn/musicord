c0876b20965e3330ac0acac56c3d8867d24ef68a
BrianWasTkn <abdg2006mhpnhs@gmail.com>
2021-01-28 14:05:24 +0800

    fix[cmds]: rework on slots.ts

--- src/commands/Utility/hl.ts
+++ src/commands/Utility/hl.ts
@@ -7,7 +7,7 @@ export default class Util extends Lava.Command {
     super('hlock', {
       aliases: ['hlock', 'hl'],
       channel: 'guild',
-      description: 'Hlocks the heist channel if you have proper permissions',
+      description: 'Locks the heist channel if you have proper permissions',
       category: 'Utility',
       userPermissions: ['MANAGE_MESSAGES']
     });
--- src/commands/Utility/hul.ts
+++ src/commands/Utility/hul.ts
@@ -8,7 +8,7 @@ export default class Util extends Lava.Command {
       aliases: ['hunlock', 'hul'],
       channel: 'guild',
       userPermissions: ['MANAGE_MESSAGES'],
-      description: 'Hunlocks the heist channel if you have right permissions',
+      description: 'Unlocks the heist channel if you have right permissions',
       category: 'Utility',
       args: [{
         id: 'role', type: 'role'
