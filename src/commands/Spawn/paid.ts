8fdcd9d00ebb4d2e1e8652025fd318feabd3ca2d
BrianWasTkn <abdg2006mhpnhs@gmail.com>
2021-02-08 13:07:06 +0800

    chore: add blacklist inhibitors, top unpaid spawns

--- src/commands/Spawn/paid.ts
+++ src/commands/Spawn/paid.ts
@@ -3,6 +3,8 @@ import {
 	Message, MessageEmbed
 } from 'discord.js'
 
+async function deleteMessage(m: Message): Promise<Message> { return m.delete({ timeout: 3000 }) };
+
 export default class Spawn extends Command {
 	public client: Akairo.Client;
 	public constructor() {
@@ -12,9 +14,13 @@ export default class Spawn extends Command {
 			category: 'Spawn',
 			userPermissions: ['MANAGE_MESSAGES'],
 			args: [{
-				id: 'amount', type: 'number', unordered: true
+				id: 'amount', 
+				type: 'number', 
+				unordered: true
 			}, {
-				id: 'user', type: 'member', unordered: true,
+				id: 'user', 
+				type: 'member', 
+				unordered: true,
 				default: (message: Message) => message.member
 			}]
 		});
@@ -23,16 +29,8 @@ export default class Spawn extends Command {
 	public async exec(_: Message, args: any): Promise<Message> {
 		const { amount, user } = args;
 		// Args
-		if (!amount) {
-			return _.reply('You need an amount.').then((m: Message) => {
-				return m.delete({ timeout: 3000 });
-			});
-		} else if (!user) {
-			return _.reply('You need a user').then((m: Message) => {
-				return m.delete({ timeout: 3000 });
-			});
-		}
-
+		if (!amount) return _.reply('You need an amount.').then(deleteMessage);
+		else if (!user) return _.reply('You need a user.').then(deleteMessage);
 		// Update
 		const data = await this.client.db.spawns.removeUnpaid(user.user.id, amount);
 		// Message
