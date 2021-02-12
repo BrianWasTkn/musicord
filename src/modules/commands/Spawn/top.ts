f7b16fb249f0d81195105086af7aba60d1f8270f
BrianWasTkn <abdg2006mhpnhs@gmail.com>
2021-02-12 12:47:56 +0800

    fix: top

--- src/modules/commands/Spawn/top.ts
+++ src/modules/commands/Spawn/top.ts
@@ -16,35 +16,37 @@ export default class Spawn extends Command {
         });
     }
 
-    private async map<T extends Lava.CurrencyProfile | Lava.SpawnDocument>(docs: (mongoose.Document & T)[], amount: number, key: string): Promise<string[]> {
+    private async map<T extends Lava.CurrencyProfile | Lava.SpawnDocument>(docs: (mongoose.Document & T)[], amount: number, key: string): Promise<Promise<string>[]> {
         let all = docs.filter((m: mongoose.Document & T) => m[key] < Infinity)
         .sort((a: (mongoose.Document & T), b: (mongoose.Document & T)) => b[key] - a[key])
-        .slice(0, amount).map(async(m: mongoose.Document & T, i: number) => {
+        .slice(0, amount);
+        
+        return all.map(async (m: mongoose.Document & T, i: number) => {
             const user = await this.client.users.fetch(m.userID);
             return (<string>(`${i + 1}. **${m[key].toLocaleString()}** - ${user.tag}`));
         });
-
-        return await Promise.all(all);
     }
 
     public async exec(_: Message, args: any): Promise<Message> {
         let { type, amount } = args;
-        type = type ?? 'unpaids'; 
-        amount ?? 10;
+        type = type || 'unpaids'; 
+        amount = amount || 10;
         
         let embed = new MessageEmbed();
         let docs: any[];
+        let mapped: Promise<string>[];
         
         if (['unpaid', 'unpaids', 'spawns', 'spawn'].includes(type)) {
             docs = await mongoose.models["spawn-profile"].find({});
-            embed.setTitle('Top Unpaids').setColor('RANDOM')
-            .setDescription((await this.map<Lava.SpawnDocument>(docs, amount, 'unpaid')).join('\n'));
+            mapped = await this.map<Lava.SpawnDocument>(docs, amount, 'unpaid');
+            embed.setTitle('Top Unpaids');
         } else if (['pocket', 'wallet'].includes(type)) {
             docs = await mongoose.models["currency"].find({});
-            embed.setTitle('Top Pockets').setColor('RANDOM')
-            .setDescription((await this.map<Lava.CurrencyProfile>(docs, amount, 'pocket')).join('\n'));
+            mapped = await this.map<Lava.SpawnDocument>(docs, amount, 'pocket');
+            embed.setTitle('Top Pockets');
         }
 
+        embed.setDescription((await Promise.all(mapped)).join('\n')).setColor('RANDOM');
         return _.channel.send({ embed });
     }
 }
