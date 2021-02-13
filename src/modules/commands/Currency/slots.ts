f90d2739c49da33b7812f9c7d0a8ab5eb29cae72
BrianWasTkn <abdg2006mhpnhs@gmail.com>
2021-02-13 16:30:04 +0800

    feat[cmds-currency]: slots rework done,

--- src/modules/commands/Currency/slots.ts
+++ src/modules/commands/Currency/slots.ts
@@ -142,20 +142,15 @@ export default class Currency extends Command {
     const { slotMachine } = this.client.config.currency;
     const rate: number[] = Object.values(slotMachine);
     const emojis: string[] = Object.keys(slotMachine);
-    console.log('Rate:', rate);
     // ty daunt
     const won: number[] = rate.map((_, i, ar) => ar[emojis.indexOf(slots[i])]).filter(Boolean);
-    console.log('Won:', won);
     const filter = (thing: string, i: number, ar: string[]) => ar.indexOf(thing) === i;
     const length = slots.filter(filter).length;
     const [emojWins] = won.filter((ew: number, i: number, a: number[]) => a.indexOf(ew) !== i);
-    console.log('Length:', length);
 
     if (length === 1 || length === 2) {
       let winnings: any = Array(length === 1 ? 3 : length).fill(emojWins) // emoji's base winnings as items
-      console.log('Winnings[]:', winnings);
-      winnings = winnings.map((e: number) => bet * (e + (e * multi))).reduce((p: number, c: number) => p + c);
-      console.log('Number Winnings:', winnings);
+      winnings = winnings.map((e: number) => bet * (e + (e * (multi / 100)))).reduce((p: number, c: number) => p + c);
       return { length, winnings: Math.round(winnings) };
     } else {
       return { length, winnings: 0 };
