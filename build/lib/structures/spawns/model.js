1598378a46fb87f5ae51bfce891a1812727906af
BrianWasTkn <abdg2006mhpnhs@gmail.com>
2021-02-16 12:27:18 +0800

    forgot to remove smh

--- build/lib/structures/spawns/model.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = require("mongoose");
-exports.default = mongoose_1.model('spawn-profile', new mongoose_1.Schema({
-    userID: { type: String, required: true },
-    unpaid: { type: Number, required: false, default: 0 },
-    eventsJoined: { type: Number, required: false, default: 0 },
-}));
