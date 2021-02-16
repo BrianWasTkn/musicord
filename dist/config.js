bbe82d71bc960d40a0af01e9db4bb2afc2933187
BrianWasTkn <abdg2006mhpnhs@gmail.com>
2021-02-16 13:07:39 +0800

    E

--- dist/config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    token: process.env.TOKEN,
-    owners: '605419747361947649',
-    prefix: 'lava'
-};
--- dist/services/ready-listener.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    id: 'ready',
-    run() {
-        const tag = `${this.user.username}#${this.user.discriminator}`;
-        return console.log(`${tag} has flown within Discord.`);
-    }
-};
