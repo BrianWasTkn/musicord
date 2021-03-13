952f8978a45090dcedc5528a9f110d17fd178173
BrianWasTkn <abdg2006mhpnhs@gmail.com>
2021-03-13 15:30:07 +0800

    fix: final

--- src/emitters/akairo/cmdFinish.ts
+++ src/emitters/akairo/cmdFinish.ts
@@ -19,27 +19,19 @@ export default class CommandListener extends Listener {
     returned: MessageOptions
   ): Promise<Message | Message[]> {
     if (!returned) return;
+
     // string-based returns
     if (typeof returned === 'string') {
-      thing.content = returned;
-      return await msg.channel.send(thing);
+      return await msg.channel.send(returned);
     }
 
-    for (const option of ['disableMentions', 'reply', 'code']) {
-      if (option in returned) {
-        thing[option] = returned[option];
+    if (!returned.embed.color) {
+      returned.embed = {
+        ...returned.embed,
+        color: this.client.util.randomColor()
       }
     }
 
-    if (returned.embed) {
-      thing.embed = returned.embed;
-      if (!returned.embed.color) {
-        thing.embed = Object.assign(thing.embed, {
-          color: this.client.util.randomColor(),
-        });
-      }
-    }
-
-    return await msg.channel.send(thing);
+    return await msg.channel.send(returned);
   }
 }
