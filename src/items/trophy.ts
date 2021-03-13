3431b7fd469a7b4720037ca1e0fbf9c65e5f3581
BrianWasTkn <abdg2006mhpnhs@gmail.com>
2021-03-13 20:28:42 +0800

    >:)

--- src/items/trophy.ts
+++ src/items/trophy.ts
@@ -12,17 +12,30 @@ export default class Collectible extends Item {
       usable: true,
       emoji: '🏆',
       name: 'Trophy',
-      info: 'Grants you another trophy if you use it.',
+      info: 'Grants you another trophy if you use it, or not.',
       cost: 2500000,
     });
   }
 
   async use(msg: Message): Promise<string> {
-    const data = await this.client.db.currency.fetch(msg.author.id);
-    data.items.find((i) => i.id === this.id).amount++;
-    await data.save();
+    const { db, util } = this.client;
+    const data = await db.currency.fetch(msg.author.id);
+    const trophies = data.items.find((i) => i.id === this.id);
+    const hahausuck = util.randomNumber(1, trophies.amount);
+    let fail: boolean;
 
+    if (Math.random() > 0.6) {
+      trophies.amount++;
+      fail = false;
+    } else {
+      fail = true;
+      trophies.amount -= hahausuck;
+    }
+
+    await data.save();
     const inv = data.items.find((i) => i.id === this.id);
-    return `You have been granted **1** trophy! You now have a total of **${inv.amount.toLocaleString()}** trophies.`;
+    return fail
+      ? `LOL You broke **${hahausuck.toLocaleString()}** trophies sucks to be you then I guess**`
+      : `You have been granted **1** trophy! You now have a total of **${trophies.amount.toLocaleString()}** trophies.`;
   }
 }
