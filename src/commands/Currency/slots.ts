e8f82080458344df353f3108e98358e5147b5eba
BrianWasTkn <abdg2006mhpnhs@gmail.com>
2021-03-14 22:20:31 +0800

    slot machine control >:D

--- src/commands/Currency/slots.ts
+++ src/commands/Currency/slots.ts
@@ -59,7 +59,7 @@ export default class Currency extends Command {
     const jOdds = Math.random() > 0.85;
     const [a, b, c] = Array(3).fill(null)
       .map(() => util.randomInArray(emojis))
-      .map((e, i, a) => jOdds ? a[0][1] : {});
+      .map((e, i, a) => jOdds ? a[0][1] : e);
 
     const outcome = `**>** :${[a, b, c].join(':    :')}: **<**`;
     // Calc amount
