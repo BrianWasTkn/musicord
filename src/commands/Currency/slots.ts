2da3b5b1bccf359b967145b792b60e267a98bfd4
BrianWasTkn <abdg2006mhpnhs@gmail.com>
2021-03-19 10:39:18 +0800

    slot rig

--- src/commands/Currency/slots.ts
+++ src/commands/Currency/slots.ts
@@ -107,7 +107,7 @@ export default class Currency extends Command {
     const jOdds = Math.random() > (0.98 - slots);
     const jEmoji = util.randomInArray(emojis);
     const temp = Array(3).fill(util.randomInArray(emojis)).map((_, i, a) => emojis.filter(e => e !== a[0][i])[i]);
-    const [a, b, c] = Array(3).fill(null).map((_, i) => (jOdds ? jEmoji : temp[i]));
+    const [a, b, c] = Array(3).fill(null).map((_, i) => (jOdds ? jEmoji : util.randomInArray(temp)[i]));
     const order = [a, b, c];
     const outcome = `**>** :${[...order].join(':    :')}: **<**`;
     let { length, winnings, multiplier = 0 } = this.calcWinnings(bet, order);
