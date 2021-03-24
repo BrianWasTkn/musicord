ef26c9aaf9475b99a02fa9e2304d01a8cc5297bd
BrianWasTkn <abdg2006mhpnhs@gmail.com>
2021-03-24 16:23:18 +0800

    feat(handlers): command queue

--- lib/utility/util.ts
+++ lib/utility/util.ts
@@ -85,16 +85,16 @@ export class Util extends ClientUtil {
   // dankmemer.lol/source
   parseTime = (time: number): string[] => {
     const methods = [
-      { name: 'd', count: 86400 },
-      { name: 'h', count: 3600 },
-      { name: 'm', count: 60 },
-      { name: 's', count: 1 }
+      { name: 'days', count: 86400 },
+      { name: 'hours', count: 3600 },
+      { name: 'minutes', count: 60 },
+      { name: 'seconds', count: 1 }
     ];
 
-    const timeStr = [ Math.floor(time / methods[0].count).toString() + methods[0].name ];
+    const timeStr = [ Math.floor(time / methods[0].count).toString() + ' ' + methods[0].name ];
     for (let i = 0; i < 3; i++) {
       const calced = Math.floor(time % methods[i].count / methods[i + 1].count);
-      timeStr.push(calced.toString() + methods[i + 1].name);
+      timeStr.push(calced.toString() + ' ' + methods[i + 1].name);
     }
 
     return timeStr.filter(g => !g.startsWith('0'));
