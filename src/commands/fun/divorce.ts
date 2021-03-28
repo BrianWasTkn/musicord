cf309bd86127e715c5288467e6df76e26fed748b
BrianWasTkn <abdg2006mhpnhs@gmail.com>
2021-03-28 22:19:18 +0800

    feat(cmds/fun): add divorce cmd

---
+++ src/commands/fun/divorce.ts
@@ -0,0 +1,43 @@
+import { MessageOptions } from 'discord.js';
+import { MessagePlus } from '@lib/extensions/message';
+import { UserPlus } from '@lib/extensions/user';
+import { Command } from '@lib/handlers/command';
+
+export default class Fun extends Command {
+	constructor() {
+		super('divorce', {
+			aliases: ['divorce'],
+			channel: 'guild',
+			description: 'Divorce your husband or wife!',
+			category: 'Fun',
+		})
+	}
+
+	async exec(msg: MessagePlus): Promise<MessageOptions> {
+		const me = await msg.author.fetchDB();
+		if (!me.marriage.id) {
+			return { replyTo: msg.id, content: 'You\'re not even married to somebody' };
+		}
+
+		const husOrWif = await this.client.users.fetch(me.marriage.id) as UserPlus;
+		await msg.channel.send(`Are you sure you wanna have a divorce with ${husOrWif.username}? Type \`y\` or \`n\` in 30 seconds.`);
+		const filt = m => m.author.id === msg.author.id;
+		const resp = (await msg.channel.awaitMessages(filt, { max: 1, time: 3e4 })).first();
+
+		if (!resp || !['yes', 'y'].includes(resp.content.toLowerCase())) {
+			return { content: 'I guess not then.' };
+		}
+
+		const div = await husOrWif.fetchDB())
+
+		delete me.marriage.id;
+		me.marriage.since = 0;
+		await me.save();
+
+		delete div.marriage.id;
+		div.marriage.since = 0;
+		await div.save();
+
+		return { replyTo: msg.id, content: `Divorce successfull.` };
+	}
+}
