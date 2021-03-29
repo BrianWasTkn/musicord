9c71a39d1dbe9f8fb1f9f5b0e7f684363be9ae70
BrianWasTkn <abdg2006mhpnhs@gmail.com>
2021-03-29 22:44:31 +0800

    feat(items): xplosive nerf

--- src/items/xplo.ts
+++ src/items/xplo.ts
@@ -28,70 +28,62 @@ export default class PowerUp extends Item {
     let odds = randomNumber(1, 100);
 
     if (odds >= 60) {
-      const mods = this.client.handlers.item.modules.array();
+      const items: { amt: number; item: Item }[] = [];
       const coins = randomNumber(5e6, 100e6);
-      let items: { amt: number; item: Item }[] = [];
-      let e = 0;
+      const mods = this.client.handlers.item.modules.array();
+      const rate = randomNumber(1, 200);
 
+      let e = 0;
       while (e <= randomNumber(3, mods.length)) {
         const item = randomInArray(
-          mods.filter((m) => !items.some((it) => it.item.id === m.id))
+          mods.filter((m) => ![...items, { 
+            amt: 0, item: mods.find(i => i.id === xplo.id) 
+          }].some((it) => it.item.id === m.id))
         );
-        items.push({ item, amt: randomNumber(2, item.cost <= 5e6 ? 500 : 10) });
+
+        const inv = data.items.find(i => i.id === item.id);
+        const amt = inv.amount + Math.round(inv.amount * (rate / 100));
+        items.push({ item, amt });
+
         e++;
       }
 
       const its = items
         .sort((a, b) => b.amt - a.amt)
-        .map(
-          ({ amt, item }) =>
-            `**__${amt.toLocaleString()}__ ${item.emoji} ${item.name}**`
-        );
-      items.forEach(
-        ({ amt, item }) =>
-          (data.items.find((i) => i.id === item.id).amount += amt)
-      );
+        .map(({ amt, item }) => `**${item.emoji} ${item.name}**`);
+      items.forEach(({ amt, item }) => {
+        data.items.find((i) => i.id === item.id).amount += amt
+      });
+
       xplo.amount--;
       await msg.author.initDB(data).addPocket(coins).updateItems().db.save();
 
-      return `**__${this.emoji} ${
-        msg.author.username
-      }'s bomb__**\n**\`${coins.toLocaleString()}\` coins**\n\n${its.join(
-        '\n'
-      )}`;
+      return `**__${this.emoji} ${msg.author.username}'s bomb__**\n**You got \`${coins.toLocaleString()}\` coins and \`+${rate}%\` of these items in your inventory:**\n\n${its.join('\n')}`;
     }
 
     const fine = randomNumber(1, data.pocket);
     const items: { amt: number; item: Item }[] = [];
     const inv = data.items.filter((i) => i.amount >= 2);
+    const rate = randomNumber(1, 100);
 
     for (let e = 0; e < inv.length; e++) {
       const mod = this.client.handlers.item.modules.get(inv[e].id);
       const it = data.items.find((i) => i.id === mod.id);
-      const amt = randomNumber(1, it.amount);
+      const amt = Math.round(it.amount * (rate / 100));
       items.push({ item: mod, amt });
     }
 
     const its = items
       .sort((a, b) => b.amt - a.amt)
-      .map(
-        ({ amt, item }) =>
-          `**__${amt.toLocaleString()}__ ${item.emoji} ${item.name}**${
-            amt > 1 ? 's' : ''
-          } lost :skull:`
-      );
-    items.forEach(
-      ({ amt, item }) =>
-        (data.items.find((i) => i.id === item.id).amount -= amt)
-    );
+      .map(({ amt, item }) => `${item.emoji} ${item.name}`);
+    items.forEach(({ amt, item }) => {
+      data.items.find((i) => i.id === item.id).amount -= amt;
+    });
+
     xplo.amount--;
     data.pocket -= fine;
     await msg.author.initDB(data).updateItems().db.save();
 
-    return `**${this.emoji} ${
-      msg.author.username
-    }'s bomb FAILED :skull:**\n**\`${fine.toLocaleString()}\` coins fined**\n\n${its.join(
-      '\n'
-    )}`;
+    return `**${this.emoji} ${msg.author.username}'s bomb FAILED :skull:**\n**You got fined \`${fine.toLocaleString()}\` coins and \`${rate}%\` of these items from your inventory**\n\n${its.join('\n')}`;
   }
 }
