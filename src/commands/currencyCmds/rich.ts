4402de3334a0c368592f7b25d71c24bf96ea4a15
BrianWasTkn <abdg2006mhpnhs@gmail.com>
2021-04-05 16:50:27 +0800

    fix: rich command

--- src/commands/currencyCmds/rich.ts
+++ src/commands/currencyCmds/rich.ts
@@ -38,9 +38,9 @@ export default class Currency extends Command {
 
     if (glob) {
       const docs = (await Mongo.models['currency'].find({})) as (Document & CurrencyProfile)[];
-      const lava = docs.filter(n => n.pocket > 0).sort((a, b) => b.pocket - a.pocket);
+      const lava = docs.filter(n => n.pocket > 0).sort((a, b) => b.pocket - a.pocket).slice(0, 10);
       const nice = await Promise.all(lava.map((l, i) => msg.client.users.fetch(l.userID, false, true).then(o => ({ o: o as UserPlus, pocket: l.pocket }))));
-      const rich = nice.map((n, i) => `:${emojis[i] || 'eggplant'}: **${n.pocket.toLocaleString()}** — ${n.o.tag}`).slice(0, 10);
+      const rich = nice.map((n, i) => `:${emojis[i] || 'eggplant'}: **${n.pocket.toLocaleString()}** — ${n.o.tag}`);
     
       return { embed: {
         author: { name: 'richest players across discord' },
@@ -53,9 +53,10 @@ export default class Currency extends Command {
     }
 
     const members = [...msg.guild.members.cache.values()];
-    const mebDoc = (await Promise.all(members.map(async m => ({ m, pocket: (await (m.user as UserPlus).fetchDB()).pocket}))));
-    const filt = mebDoc.filter(m => m.pocket > 0).sort((a, b) => b.pocket - a.pocket).slice(0, 10);
-    const rich = filt.map((n, i) => `:${emojis[i] || 'eggplant'}: **${n.pocket.toLocaleString()}** — ${n.m.user.tag}`);
+    const mebDoc = (await Promise.all(members.map(async m => await Mongo.models['currency'].findOne({ userID: m.user.id })))).filter(Boolean) as (Document & CurrencyProfile)[];
+    const abcde = mebDoc.filter(m => m.pocket > 0).sort((a, b) => b.pocket - a.pocket);
+    const filt = await Promise.all(abcde.map(async a => ({ pocket: a, m: await msg.client.users.fetch(a.userID, false, true) })));
+    const rich = filt.map((n, i) => `:${emojis[i] || 'eggplant'}: **${n.pocket.toLocaleString()}** — ${n.m.tag}`);
 
     return { embed: {
       author: { name: 'richest players in this server' },
