576eae00915f11f8dc8858113cb3f56f76f76813
BrianWasTkn <abdg2006mhpnhs@gmail.com>
2021-04-07 17:38:22 +0800

    feat(lotto): add lottery

--- config/lottery.ts
+++ config/lottery.ts
@@ -12,9 +12,10 @@ export interface LottoConfig {
 
 export const lottoConfig: LottoConfig = {
 	guildID: '691416705917779999',
-	channelID: '717351680676462712',
+	// channelID: '717351680676462712',
+	channelID: '809489910351921192',
 	requirementID: '692517500814098462',
-	interval: 1000 * 60 * 60 * 6,
+	interval: 1000 * 1e4,
 	rewards: {
 		min: 100000,
 		max: 1000000,
