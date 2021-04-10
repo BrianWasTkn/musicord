ab383f1668879829d4d14d47d2b7119432f3f145
BrianWasTkn <abdg2006mhpnhs@gmail.com>
2021-04-10 18:41:02 +0800

    chore: Release v2.15.3

--- src/commands/spawnCmds/rich.ts
+++ src/commands/spawnCmds/rich.ts
@@ -28,7 +28,7 @@ export default class Spawn extends Command {
 
     const docs = (await Mongo.models['spawn-profile'].find({})) as (Document & SpawnDocument)[];
     const filt = docs.filter(s => s.unpaid < Infinity && s.unpaid > 0).sort((a, b) => b.unpaid - a.unpaid).slice(0, count);
-    const rich = filt.map(async (f, i) => {
+    const rich = filt.map((f, i) => {
       const user = msg.guild.members.cache.get(f.userID);
       return `:${emojis[i] || 'eggplant'}: **${f.unpaid.toLocaleString()}** - ${user.user.tag || 'LOL WHO DIS'}`;
     });
