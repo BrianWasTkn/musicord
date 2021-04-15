4230b3e871f84306f9906f777464c7ffe6f06169
BrianWasTkn <abdg2006mhpnhs@gmail.com>
2021-04-15 19:31:48 +0800

    chore: Release v2.15.5

--- src/emitters/client/clientCultUpdate.ts
+++ src/emitters/client/clientCultUpdate.ts
@@ -4,7 +4,7 @@ import { Listener } from '@lib/handlers';
 const cults = {
 	probber: '790379642359119902',
 	chips: '824984193170931782',
-	wastaken: '768812126633984021 ',
+	wastaken: '768812126633984021',
 };
 
 export default class ClientListener extends Listener {
@@ -19,13 +19,17 @@ export default class ClientListener extends Listener {
   	if (o.nickname === n.nickname) return;
   	if (n.partial) await n.fetch(true);
 
-  	const keys = Object.keys(cults);
   	const roles = Object.values(cults);
-  	const match = n.guild.roles.cache.get(keys.find(k => n.nickname.toLowerCase().includes(k)));
-  	if (match) {
-  		await n.roles.add(match.id);
+  	const keys = Object.keys(cults);
+
+  	const matchingRoles = [...n.guild.roles.cache.values()]
+  	.filter(r => n.nickname.toLowerCase().includes(r.name));
+
+  	if (matchingRoles.length >= 1) {
+  		// Promise.race big flex
+  		await Promise.race([...matchingRoles.map(r => n.roles.add(r.id))]);
   	} else {
-  		await Promise.all([...roles.map(r => n.roles.remove(r))])
+  		await Promise.all([...roles.map(r => n.roles.remove(r))]);
   	}
   }
 }
