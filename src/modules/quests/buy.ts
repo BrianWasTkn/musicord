e3684587fe997a56fbe2c61d1a000c2a6e614295
BrianWasTkn <abdg2006mhpnhs@gmail.com>
2021-05-18 19:06:10 +0800

    feat: get ready for rewrite

--- src/modules/quests/buy.ts
+++ src/modules/quests/buy.ts
@@ -3,7 +3,7 @@ import { Quest } from 'lib/objects';
 export default class Difficult extends Quest {
     constructor() {
         super('buy',{
-            rewards: { coins: 5e5, item: [5000, 'crazy'], },
+            rewards: { coins: 15e5, item: [50, 'crazy'], },
             target: [1e5, 'buy', 'buyItem'],
             difficulty: 'Difficult',
             info: 'Buy 100,000 pieces of any item type.',
--- src/modules/quests/fuck.ts
+++ src/modules/quests/fuck.ts
@@ -3,8 +3,8 @@ import { Quest } from 'lib/objects';
 export default class Easy extends Quest {
 	constructor() {
 		super('fuck', {
-			rewards: { coins: 1e4, item: [10, 'thicm'] },
-			target: [100, 'sell', 'sellItem'],
+			rewards: { coins: 1e4, item: [50, 'thicm'] },
+			target: [100000, 'sell', 'sellItem'],
 			difficulty: 'Easy',
 			info: 'Sell 100 pieces of any item type.',
 			name: 'Fuck It',
