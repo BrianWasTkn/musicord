1d34a8f1027741083d7eec7f592bae7f0f013dec
BrianWasTkn <abdg2006mhpnhs@gmail.com>
2021-05-18 18:19:35 +0800

    feat: get ready for rewrite

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import 'module-alias/register';
 import 'dotenv/config';
 import Instance from './instance';
 import config from 'config/index';
-import './sharder';
+// import './sharder';
 
 const { akairo, discord, handlers, bot: { mongo: { uri, options } } } = config;
 const lava = Instance(akairo, discord, handlers);
