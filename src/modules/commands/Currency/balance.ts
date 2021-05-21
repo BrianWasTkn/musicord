cb8f284b76813686ce7f3a69d4dd2910ac142dde
BrianWasTaken <abdg2006mhpnhs@gmail.com>
2021-05-21 16:55:37 +0800

    chore[bot-rewrite]: Release v3.0.01

--- src/modules/commands/Currency/balance.ts
+++ src/modules/commands/Currency/balance.ts
@@ -20,6 +20,12 @@ export default class Currency extends Command {
 	public async exec(ctx: Context<{ member: GuildMember }>): Promise<MessageOptions> {
 		const isContextUser = ctx.author.id === ctx.args.member.user.id;
 		const { props } = await ctx.getUser(ctx.args.member.user.id).currency();
-		return { content: props.pocket.toLocaleString() };
+		return { embed: {
+			title: `${ctx.args.member.user.username}'s balance`,
+			color: Math.random() * 0xabcdef, description: Object.entries({
+				Pocket: props.pocket.toLocaleString(),
+				Vault: props.vault.toLocaleString()
+			}).map(([f, v]) => `**${f}:** ${v}`)
+		}};
 	};
 }
