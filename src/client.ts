e7f364bfd8aa649d1ef4d7a63cd6aebef2f566bc
BrianWasTaken <abdg2006mhpnhs@gmail.com>
2021-05-26 14:55:27 +0800

    chore: add cooldown collection

--- src/client.ts
+++ src/client.ts
@@ -1,12 +1,23 @@
 import 'module-alias/register';
+import 'dotenv/config';
 
-import { LavaClient, CommandHandler, ListenerHandler, Command, Listener } from './library';
+import { LavaClient, CommandHandler, ListenerHandler, ItemHandler, Command, Listener, Item } from './library';
 import { ClientOptions, Intents } from 'discord.js';
 import { join } from 'path';
 
 const bot = new LavaClient({ 
 	ownerID: ['605419747361947649'],
-	intents: Intents.ALL 
+	intents: Intents.ALL,
+	messageCacheLifetime: 20,
+	messageCacheMaxSize: 100,
+	messageSweepInterval: 30,
+	presence: {
+		status: 'dnd',
+		activities: [
+			{ type: 'WATCHING', name: 'things load...' },
+			{ type: 'WATCHING', name: 'volcanos erupt...' }
+		]
+	} 
 });
 
 bot.listenerHandler = new ListenerHandler(bot, {
@@ -18,14 +29,27 @@ bot.listenerHandler = new ListenerHandler(bot, {
 
 bot.commandHandler = new CommandHandler(bot, { 
 	directory: join(__dirname, 'modules', 'commands'),
+	automateCategories: true,
 	classToHandle: Command,
 	useNames: true,
 	prefix: ['lava'],
 }).loadAll().useListenerHandler(bot.listenerHandler);
 
+bot.itemHandler = new ItemHandler(bot, {
+	directory: join(__dirname, 'modules', 'items'),
+	automateCategories: true,
+	classToHandle: Item,
+	useNames: true,
+}).loadAll();
+
 bot.once('ready', () => {
-	bot.console.log('Client', `${bot.listenerHandler.modules.size} Listeners Loaded`);
-	bot.console.log('Client', `${bot.commandHandler.modules.size} Commands Loaded`);
+	const messages = [
+		`${bot.listenerHandler.modules.size} Listeners Loaded`,
+		`${bot.commandHandler.modules.size} Commands Loaded`,
+		`${bot.itemHandler.modules.size} Items Loaded`,
+	];
+	
+	for (const message of messages) bot.console.log('Client', message);
 });
 
 bot.connect();
