79236d8133f2e48fb88202bf52375e6390fcb46a
BrianWasTaken <abdg2006mhpnhs@gmail.com>
2021-06-10 22:53:07 +0800

    hush

--- src/library/akairo/command/CommandHandler.ts
+++ src/library/akairo/command/CommandHandler.ts
@@ -143,6 +143,7 @@ export class CommandHandler extends OldCommandHandler implements AbstractHandler
 				this.emit(CommandHandlerEvents.COMMAND_FINISHED, context, command, args);
 			} catch (error) {
 				this.emit('commandError', context, command, args, error);
+				console.error(error);
 			} finally {
 				this.commandQueue.next(context.author.id);
 			}
