6e25fd169b3efc0ae0ed23244c325e1c4a99f56c
BrianWasTaken <abdg2006mhpnhs@gmail.com>
2021-06-15 14:13:44 +0800

    feat(Command): add rich

---
+++ src/plugins/command/modules/Currency/rich.ts
@@ -0,0 +1,28 @@
+import { Command, Context, CurrencyModel, CurrencyEntry } from 'lava/index';
+
+export default class extends Command {
+	constructor() {
+		super('rich', {
+			aliases: ['rich'],
+			clientPermissions: ['EMBED_LINKS'],
+			description: 'View the top rich users of currency.',
+			name: 'Rich'
+		});
+	}
+
+	top(amount = 10) {
+		const docs = CurrencyModel.find({}).sort({ 'props.pocket': 'desc' }).exec();
+		return docs.then(docs => docs.map(doc => new CurrencyEntry(this.client, doc)));
+	}
+
+	async exec(ctx: Context) {		
+		return ctx.channel.send({ embed: {
+			author: { name: 'richest users in this server' },
+			color: 'BLUE', description: await this.top()
+				.then(docs => docs.map(doc => {
+					const user = ctx.client.users.cache.get(doc._id)?.tag ?? 'LOL WHO DIS';
+					return `**:coin: ${doc.props.pocket}** — ${user}`;
+				}).join('\n'))
+		}});
+	}
+}
