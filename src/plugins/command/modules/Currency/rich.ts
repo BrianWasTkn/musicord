a85203f9f79aef756121ce522c22999c3591c3c0
BrianWasTaken <abdg2006mhpnhs@gmail.com>
2021-06-15 22:42:26 +0800

    fix(Command): rich command localization

--- src/plugins/command/modules/Currency/rich.ts
+++ src/plugins/command/modules/Currency/rich.ts
@@ -1,5 +1,5 @@
 import { Command, Context, CurrencyModel, CurrencyEntry } from 'lava/index';
-import { Snowflake } from 'discord.js';
+import { Snowflake, GuildMember, Collection } from 'discord.js';
 
 export default class extends Command {
 	constructor() {
@@ -11,15 +11,18 @@ export default class extends Command {
 		});
 	}
 
-	top(amount = 10) {
+	top(members: Collection<Snowflake, GuildMember>, amount = 10) {
 		const docs = CurrencyModel.find({}).sort({ 'props.pocket': 'desc' }).exec();
-		return docs.then(docs => docs.map(doc => new CurrencyEntry(this.client, doc)).filter(doc => doc.props.pocket > 0).slice(0, amount));
+		return docs.then(docs => docs.map(doc => new CurrencyEntry(this.client, doc))
+			.filter(doc => members.has(doc.data._id as Snowflake))
+			.filter(doc => doc.props.pocket > 0)
+			.slice(0, amount));
 	}
 
 	async exec(ctx: Context) {		
 		return ctx.channel.send({ embed: {
 			author: { name: 'richest users in this server' },
-			color: 'ORANGE', description: await this.top()
+			color: 'ORANGE', description: await this.top(ctx.guild.members.cache)
 				.then(docs => docs.map((doc, i) => {
 					const user = ctx.client.users.cache.get(doc.data._id as Snowflake)?.tag ?? 'LOL WHO DIS';
 					const emoji = Array(3).fill('fire')[i] ?? 'coin';
