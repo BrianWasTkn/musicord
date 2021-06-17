62ea0e2b8853d728196bd9a92e21d18972358c2e
BrianWasTaken <abdg2006mhpnhs@gmail.com>
2021-06-17 21:00:57 +0800

    fix: that culprit

--- src/plugins/command/modules/Currency/rich.ts
+++ src/plugins/command/modules/Currency/rich.ts
@@ -25,7 +25,7 @@ export default class extends Command {
 			color: 'RED', description: await this.top(ctx.guild.members.cache)
 				.then(docs => docs.map((doc, i) => {
 					const user = ctx.client.users.cache.get(doc.data._id as Snowflake)?.tag ?? 'LOL WHO DIS';
-					const emoji = Array(3).fill('small_red_triangle')[i] ?? 'coin';
+					const emoji = Array(3).fill('coin')[i] ?? 'small_red_triangle';
 					return `**:${emoji}: ${doc.props.pocket.toLocaleString()}** — ${user}`;
 				}).join('\n'))
 		}});
