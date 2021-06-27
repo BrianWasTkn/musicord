7d65008e7b18d58958b43c9c1e26de31ef31e35f
BrianWasTaken <abdg2006mhpnhs@gmail.com>
2021-06-27 17:08:00 +0800

    github more like bughub

--- src/framework/LavaClient.ts
+++
@@ -1,6 +0,0 @@
-import { Client, Collection } from 'discord.js';
-import { LavaHandler } from './LavaHandler';
-
-export class LavaClient extends Client {
-	// public constructor(ClientOptions & LavaOptions)
-}
--- src/framework/LavaHandler.ts
+++
@@ -1,7 +0,0 @@
-import { EventEmitter } from 'events';
-import { LavaModule } from './LavaModule';
-import { Collection } from 'discord.js';
-
-export class LavaHandler extends EventEmitter {
-	public modules: Collection<string, LavaModule>;
-}
