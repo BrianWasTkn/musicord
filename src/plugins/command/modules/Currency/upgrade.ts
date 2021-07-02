7e79774cde2403c5de2e8fb61e1427c0a18a56df
BrianWasTaken <abdg2006mhpnhs@gmail.com>
2021-07-02 16:53:05 +0800

    chore(Command): upgrade command functionality

--- src/plugins/command/modules/Currency/upgrade.ts
+++ src/plugins/command/modules/Currency/upgrade.ts
@@ -5,6 +5,7 @@ export default class extends Command {
 		super('upgrade', {
 			aliases: ['upgrade', 'upg'],
 			clientPermissions: ['EMBED_LINKS'],
+			cooldown: 1000 * 60 * 1,
 			description: 'Upgrade an item!',
 			name: 'Upgrade',
 			args: [
@@ -17,6 +18,14 @@ export default class extends Command {
 	}
 
 	async exec(ctx: Context, { item }: { item: Item }) {
-		
+		const choice = await ctx.awaitMessage();
+		await ctx.channel.send(`What item do u wanna upgrade?`);
+		if (!choice || !choice.content) {
+			await ctx.reply('ok no then weirdo');
+			return false;
+		}
+
+		await ctx.reply('Sike, upgrade comin later ok?');
+		return false;
 	}
 }
