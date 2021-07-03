49088fc7ac9486f42dc7c3a5dc1d82ce16a078bb
BrianWasTaken <abdg2006mhpnhs@gmail.com>
2021-07-03 14:53:09 +0800

    chore: release v3.0.19

--- src/plugins/command/modules/Currency/upgrade.ts
+++ src/plugins/command/modules/Currency/upgrade.ts
@@ -19,31 +19,33 @@ export default class extends Command {
 
 	async exec(ctx: Context, { item }: { item: Item }) {
 		const entry = await ctx.currency.fetch(ctx.author.id);
+		if (!item) return ctx.reply('You need to upgrade something!').then(() => false);
+
 		const inv = entry.items.get(item.id);
 		const isMax = item.upgrades.length - 1 === inv.level;
+		const { upgrade, icon, premium } = inv.upgrade;
+		const e = premium ? 'keys' : 'coins';
 
 		if (inv.upgrade.upgrade > (item.premium ? entry.props.prem : entry.props.pocket)) {
-			const { upgrade, icon, premium } = inv.upgrade;
-			const e = premium ? 'keys' : 'coins';
-			return ctx.reply(`You need **${upgrade.toLocaleString()} ${icon} ${e}** to upgrade this item!`).then(() => false);
+			return ctx.reply(`You need **${icon} ${upgrade.toLocaleString()} ${e}** to upgrade this item!`).then(() => false);
 		}
 		if (isMax) {
 			return ctx.reply(`Your **${inv.upgrade.emoji} ${inv.upgrade.name}** is already at max level!`).then(() => false);
 		}
 
-		await ctx.channel.send({ embed: { description: `Are you sure you wanna upgrade your **${item.emoji} ${item.name}** to **Level ${inv.level + 1}** for **${inv.upgrade.upgrade}** ${inv.upgrade.premium ? 'keys' : 'coins'}?` }});
+		await ctx.channel.send({ embed: { color: 'ORANGE', description: `Are you sure you wanna upgrade your **${item.emoji} ${item.name}** to **Level ${inv.level + 1}** for **${upgrade.toLocaleString()} ${e}** right now?` }});
 		const choice = await ctx.awaitMessage();
 		if (!choice || !choice.content) {
-			return ctx.reply({ embed: { description: 'Imagine not answering to me lmfaooo' }}).then(() => false);
+			return ctx.reply({ embed: { color: 'RED', description: 'Imagine not answering to me lmfaooo' }}).then(() => false);
 		}
 		if (choice.content.toLowerCase().slice(0, 1) === 'n') {
-			return ctx.reply({ embed: { description: 'ok then.' }}).then(() => false);
+			return ctx.reply({ embed: { color: 'INDIGO', description: 'ok then.' }}).then(() => false);
 		}
 
 		const newInv = await entry.removePocket(inv.upgrade.upgrade).upgradeItem(item.id).save().then(e => e.items.get(item.id));
 		return ctx.reply({ embed: {
-			color: 'BLUE', author: { 
-				name: `${inv.upgrade.name} reached ${
+			color: 'GREEN', author: { 
+				name: `${inv.upgrade.name} finally reached ${
 					isMax ? 'max level' : `level ${newInv.level}`
 				}!`,
 				iconURL: ctx.author.avatarURL({ 
