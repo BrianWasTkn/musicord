da3bc0f1664f72b322a0a17a3a7057c1c71ea826
BrianWasTaken <abdg2006mhpnhs@gmail.com>
2021-07-06 18:25:30 +0800

    feat(Command): postmeme functionality

--- src/plugins/command/categories/GambleCommand.ts
+++ src/plugins/command/categories/GambleCommand.ts
@@ -73,7 +73,7 @@ export class GambleCommand extends Command {
 	}
 
 	calcWinnings(multi: number, bet: number) {
-		const winnings = Math.ceil(bet * (Math.random() * 5));
+		const winnings = Math.ceil(bet * (Math.random() * 3));
 		return Math.min(Currency.MAX_WIN, winnings + Math.ceil(winnings * (multi / 100)));
 	}
 }
