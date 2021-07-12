222a14363a4108a535f61c08119e9ff6f1128bdd
BrianWasTaken <abdg2006mhpnhs@gmail.com>
2021-07-12 13:22:31 +0800

    feat(mongo): name ruling

--- src/library/mongo/currency/endpoint.ts
+++ src/library/mongo/currency/endpoint.ts
@@ -12,20 +12,22 @@ export interface CurrencyEndpointEvents {
 	coinShare: [entry: CurrencyEntry, user: UserPlus, args: { sharer: UserPlus; coins: number; }];
 	/** Emitted when someone gifts items. */
 	itemGift: [entry: CurrencyEntry, user: UserPlus, args: { gifter: UserPlus; amount: number }];
-	/** Emitted when they leveld up. */
+	/** Emitted when they leveled up. */
 	levelUp: [entry: CurrencyEntry, user: UserPlus, /** args: { rewards: { multi?: number; coins?: number; keys?: number; items?: [number, string] } } */]
 }
 
-export class CurrencyEndpoint extends Endpoint<CurrencyProfile> {
+export interface CurrencyEndpoint extends Endpoint<CurrencyProfile> {
 	/** 
 	 * Listen for currency events. 
 	 */
-	public on: <K extends keyof CurrencyEndpointEvents>(event: K, listener: (...args: CurrencyEndpointEvents[K]) => Awaited<void>) => this;
+	on: <K extends keyof CurrencyEndpointEvents>(event: K, listener: (...args: CurrencyEndpointEvents[K]) => Awaited<void>) => this;
 	/**
 	 * Emit currency events.
 	 */
-	public emit: <K extends keyof CurrencyEndpointEvents>(event: K, ...args: CurrencyEndpointEvents[K]) => boolean;
+	emit: <K extends keyof CurrencyEndpointEvents>(event: K, ...args: CurrencyEndpointEvents[K]) => boolean;
+}
 
+export class CurrencyEndpoint extends Endpoint<CurrencyProfile> {
 	/** 
 	 * Fetch something from the db. 
 	 */
