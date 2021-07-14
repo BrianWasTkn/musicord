d6e67d8c9f47264275f7718443ec7e64ca8d9a9a
BrianWasTaken <abdg2006mhpnhs@gmail.com>
2021-07-14 15:28:05 +0800

    ok

--- src/plugins/command/modules/Staff/pping.ts
+++ src/plugins/command/modules/Staff/pping.ts
@@ -23,21 +23,13 @@ export default class extends SubCommand {
 
 	async exec(ctx: Context, { msg }: { msg: string }) {
 		const role = ctx.guild.roles.cache.get('857158450890801152');
+
 		await ctx.delete();
-		return await ctx.channel.send({ 
-			content: role.toString(), 
-			allowedMentions: {
-				roles: [role.id]
-			},
-			embed: {
-				footer: {
-					text: ctx.author.username,
-					iconURL: ctx.author.avatarURL({ dynamic: true })
-				},
-				description: msg,
-				color: 'GREEN',
-				title: ':handshake: Partners :handshake:'
-			}
-		}).then(() => false);
+		await ctx.channel.send({ 
+			content: [`:handshake: ${role.toString()}`, msg].join('\n'), 
+			allowedMentions: { roles: [role.id] },
+		});
+
+		return false;
 	}
 }
